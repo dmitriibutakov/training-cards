6c8f04b7b9fa6cae2a59ae5e7c08859842fccebc
VadymHromyk <hromuk.vadim@gmail.com>
2022-07-28 18:04:18 +0300

    1

--- src/3_commons/common_images/commonImages.ts
+++ src/3_commons/common_images/commonImages.ts
@@ -1,3 +1,5 @@
 import show from "./eye.png"
+import unknown_avatar from "./unknown_avatar.png"
 
 export const showImg = show
+export const unknownAvatarImg = unknown_avatar
--- src/4_components/Main/Profile/Profile.tsx
+++ src/4_components/Main/Profile/Profile.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import privateClass from "../../../3_commons/common_classes/commonContainer.module.css"
+import UniversalBtn from '../../../3_commons/common_components/UniversalBtn/UniversalBtn';
+import UniversalInput from '../../../3_commons/common_components/UniversalInput/UniversalInput';
 import UniversalTitle from "../../../3_commons/common_components/UniversalTitle/UniversalTitle";
+import { unknownAvatarImg } from '../../../3_commons/common_images/commonImages';
 
 const Profile = () => {
     return (
         <div className={privateClass.container}>
-            <UniversalTitle title={'Profile'}/>
+            <UniversalTitle title={'Personal Information'}/>
+            <img src={unknownAvatarImg} />
+            <UniversalInput placeholder='Nickname'></UniversalInput>
+            <UniversalInput placeholder='Email'></UniversalInput>
+            <UniversalBtn text='Cancel'></UniversalBtn>
+            <UniversalBtn text='Save'></UniversalBtn>
         </div>
     );
 };
